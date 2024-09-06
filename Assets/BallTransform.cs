d47fd3adf54361a9067ecde0b12890a09f809d10
kangdenglin <kangdenglin@outlook.com>
2024-09-06 04:27:51 -0700

    updated ball movement

--- Assets/BallTransform.cs
+++ Assets/BallTransform.cs
@@ -15,6 +15,6 @@ public class TrackPosition : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position += positionChange;
+        transform.localScale += positionChange;
     }
 }
